9d20343a4290b6b3a07fe5b704d2c4273243bf86
Danilo Jose Marques Pinto <danilo.pinto@axestecnologia.com.br>
2025-03-27 10:31:21 -0400

    Adicionando códigos

---
+++ script.js
@@ -0,0 +1,43 @@
+  // aplicação da barra lateral mobile
+    let btnMenu = document.getElementById('btn-menu')
+    let menu = document.getElementById('menu-mobile')
+    let overlay = document.getElementById('overlay-menu')
+
+    btnMenu.addEventListener('click', ()=>{
+        menu.classList.add('abrir-menu')
+    })
+
+    menu.addEventListener('click', ()=>{
+        menu.classList.remove('abrir-menu')
+    })
+
+    overlay.addEventListener('click', ()=>{
+        menu.classList.remove('abrir-menu')
+    })
+    // final da aplicação lateral mobile
+
+
+    const form = document.getElementById('contactForm');
+    form.addEventListener('submit', async (e) => {
+      e.preventDefault();
+      const formData = new FormData(form);
+      const data = Object.fromEntries(formData.entries());
+  
+      try {
+        const response = await fetch('https://api-drab-nu-62.vercel.app/api/server', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(data),
+        });
+  
+        if (response.ok) {
+          alert('Formulário enviado com sucesso!');
+        } else {
+          alert('Erro ao enviar o formulário.');
+        }
+      } catch (error) {
+        console.error(error);
+        alert('Erro ao enviar o formulário.');
+      }
+    });
+    
